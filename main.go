2abecead56f704df4a2ccb4838d6cc8b2d941cf4
Devatoria <joris.bonnefoy@gmail.com>
2017-09-27 15:01:19 +0200

    Upgrade to mesos 1.3.1

--- main.go
+++ main.go
@@ -13,20 +13,24 @@ import (
 
 var (
 	agentEndpoint           string
+	cgroupsEnableCFS        bool
 	containerName           string
 	docker                  string
 	dockerSocket            string
 	executorID              string
+	externalLogFile         string
 	frameworkID             string
 	help                    bool
 	initializeDriverLogging bool
 	launcherDir             string
 	logBufSecs              uint
+	logDir                  string
 	loggingLevel            string
 	mappedDirectory         string
 	quiet                   bool
 	sandboxDirectory        string
 	stopTimeout             string
+	taskEnvironment         []string
 )
 
 var rootCmd = &cobra.Command{
@@ -75,18 +79,22 @@ func init() {
 	cobra.OnInitialize(readConfig)
 
 	// Flags given by the agent when running th executor
+	rootCmd.PersistentFlags().BoolVar(&cgroupsEnableCFS, "cgroups_enable_cfs", false, "Cgroups feature flag to enable hard limits on CPU resources via the CFS bandwidth limiting subfeature")
 	rootCmd.PersistentFlags().StringVar(&containerName, "container", "", "Container name")
 	rootCmd.PersistentFlags().StringVar(&docker, "docker", "", "Docker executable path (unused)")
 	rootCmd.PersistentFlags().StringVar(&dockerSocket, "docker_socket", "", "Docker socket path")
+	rootCmd.PersistentFlags().StringVar(&externalLogFile, "external_log_file", "", "File to log in if not using the default system")
 	rootCmd.PersistentFlags().BoolVar(&help, "help", false, "Prints the help message (unused)")
 	rootCmd.PersistentFlags().BoolVar(&initializeDriverLogging, "initialize_driver_logging", true, "This option has no effect when using the HTTP scheduler/executor APIs")
 	rootCmd.PersistentFlags().StringVar(&launcherDir, "launcher_dir", "", "Folder from where the executor is launched")
 	rootCmd.PersistentFlags().UintVar(&logBufSecs, "logbufsecs", 0, "Maximum number of seconds that logs may be buffered for.")
+	rootCmd.PersistentFlags().StringVar(&logDir, "log_dir", "", "Location to put log files")
 	rootCmd.PersistentFlags().StringVar(&loggingLevel, "logging_level", "", "Logging level")
 	rootCmd.PersistentFlags().StringVar(&mappedDirectory, "mapped_directory", "", "The sandbox directory path that is mapped in the docker container")
 	rootCmd.PersistentFlags().BoolVar(&quiet, "quiet", false, "Disable logging to stderr")
 	rootCmd.PersistentFlags().StringVar(&sandboxDirectory, "sandbox_directory", "", "The path to the container sandbox holding stdout and stderr files into which docker container logs will be redirected")
 	rootCmd.PersistentFlags().StringVar(&stopTimeout, "stop_timeout", "", "Time to wait before killing a currently stopping container")
+	rootCmd.PersistentFlags().StringSliceVar(&taskEnvironment, "task_environment", []string{}, "A JSON map of environment variables and values that should be passed into the task launched by this executor.")
 
 	// Custom flags
 	rootCmd.PersistentFlags().Bool("debug", true, "Enable debug mode")
